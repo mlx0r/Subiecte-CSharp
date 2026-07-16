cbf4be2a0dc13a9d65dd8e25a66f81f3ce2952a0
Rares Chelariu <rareschelariu79@gmail.com>
2026-07-16 20:10:11 +0300

    feat: persoana

---
+++ 2012/Matrimoniale/Matrimoniale/Persoana.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Matrimoniale;
+
+public class Persoana
+{
+    public Persoana(string nume, string prenume, string ocupatie, string sex, decimal varsta)
+    {
+        Nume = nume;
+        Prenume = prenume;
+        Ocupatie = ocupatie;
+        Sex = sex;
+        Varsta = varsta;
+    }
+
+    public string Nume { get; }
+    public string Prenume { get; }
+    public string Ocupatie { get; }
+    public string Sex { get; }
+    public decimal Varsta { get; }
+}
